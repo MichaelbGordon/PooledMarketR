42164bfd245ce9f0dbf97598cf19de4a360908f6
Michael Gordon <m.b.gordon@massey.ac.nz>
2019-05-06 09:23:24 +1200

    Inital Package Commit

---
+++ R/hello.R
@@ -0,0 +1,29 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
+
+# library(tidyverse)
+# pm_data <- read_rds('/Users/mbgordon/Dropbox/Michael_shared/pooled\ market/Final\ Data/Pooled_Market_Data_full_set.rds')
+# save(pm_data,file = 'data/pm_data.RData')
+#
+# load(file = 'data/PM_data.RData')
+# library(devtools)
+# t <- PooledMarketR::object
+# t <- PM_data
+# build()
+#t <- PooledMarketR::pm_data
